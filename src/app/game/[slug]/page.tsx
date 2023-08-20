0c17f65b9f56ff1bd374288752c2dd87b5734fa1
flisowna <ewa.agnieszka.flis@gmail.com>
2023-08-20 20:08:54 +0200

    commit for new deployment

--- src/app/game/[slug]/page.tsx
+++ src/app/game/[slug]/page.tsx
@@ -2,6 +2,7 @@ import {getData} from '../../../../utils/api';
 import Game from '@/app/components/Game';
 
 export default async function Home({ params }: { params: { slug: string } }) {
+  
   const data = await getData(params.slug)
 
   return (
