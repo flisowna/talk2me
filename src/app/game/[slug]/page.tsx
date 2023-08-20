002adcf99ccd9b74fd449ae24e20cd33aedd6a84
flisowna <ewa.agnieszka.flis@gmail.com>
2023-08-20 21:07:34 +0200

    fix: move generateStaticParams to actual Game page component

--- src/app/game/[slug]/page.tsx
+++ src/app/game/[slug]/page.tsx
@@ -1,8 +1,17 @@
 import {getData} from '../../../../utils/api';
 import Game from '@/app/components/Game';
+import pathsToAPITableIds from '../../../../utils/mapping';
+
+
+export async function generateStaticParams() {
+ 
+  return Object.keys(pathsToAPITableIds).map((path) => ({
+    slug: path,
+  }))
+}
 
 export default async function Home({ params }: { params: { slug: string } }) {
-  
+
   const data = await getData(params.slug)
 
   return (
--- utils/api.ts
+++ utils/api.ts
@@ -1,48 +1,41 @@
-import pathsToAPITableIds from './mapping';
+import pathsToAPITableIds from "./mapping";
 
 const API_TOKEN = process.env.NEXT_PUBLIC_API_TOKEN;
 
-export async function generateStaticParams() {
- 
-  return Object.keys(pathsToAPITableIds).map((path) => ({
-    slug: path,
-  }))
-}
-
-export async function getData( slug: string ) {
-  const path = pathsToAPITableIds[slug]
-  const fullApiUrl = `https://apitable.com/fusion/v1/datasheets/${path}/records`
-  console.log('fullApiUrl', fullApiUrl);
-  const res = await fetch(fullApiUrl,
-    {
+export async function getData(slug: string) {
+  const path = pathsToAPITableIds[slug];
+  const fullApiUrl = `https://apitable.com/fusion/v1/datasheets/${path}/records`;
+  console.log("fullApiUrl", fullApiUrl);
+  const res = await fetch(fullApiUrl, {
     headers: {
-        Authorization: `Bearer ${API_TOKEN}`,
-  },
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
     // cache: 'no-store'
+  });
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
 
-  }    )
-if (!res.ok) {
-  throw new Error('Failed to fetch data')
-}
+  const data = await res.json();
+  const transformedData = data.data.records.map(
+    (record: { recordId: any; fields: { [x: string]: any } }) => ({
+      id: record.fields["Question ID"],
+      questionText: record.fields["Question text"],
+      answerA: record.fields["Answer A Text"],
+      feedbackA: {
+        title: record.fields["Title - Feedback to Answer A"],
+        text: record.fields["Text - Feedback to Answer A"],
+      },
+      answerALeadsTo: record.fields["Answer A leads to"],
+      answerB: record.fields["Answer B Text"],
+      feedbackB: {
+        title: record.fields["Title - Feedback to Answer B"],
+        text: record.fields["Text - Feedback to Answer B"],
+      },
+      answerBLeadsTo: record.fields["Answer B leads to"],
+      gameOverText: record.fields["Game Over Text"],
+    })
+  );
 
-const data = await res.json();
-const transformedData = data.data.records.map((record: { recordId: any; fields: { [x: string]: any; }; }) => ({
-  id: record.fields['Question ID'],
-  questionText: record.fields['Question text'],
-  answerA: record.fields['Answer A Text'],
-  feedbackA: {
-    title: record.fields['Title - Feedback to Answer A'],
-    text: record.fields['Text - Feedback to Answer A'],
-  },
-  answerALeadsTo: record.fields['Answer A leads to'],
-  answerB: record.fields['Answer B Text'],
-  feedbackB: {
-    title: record.fields['Title - Feedback to Answer B'],
-    text: record.fields['Text - Feedback to Answer B'],
-  },
-  answerBLeadsTo: record.fields['Answer B leads to'],  
-  gameOverText: record.fields['Game Over Text'],
-}));
-
-return transformedData;
+  return transformedData;
 }
